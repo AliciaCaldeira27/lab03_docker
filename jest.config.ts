10299f8fd655e84358a7323a9e38bc972acd5a56
aliciacaldeira42@gmail.com <aliciacaldeira27@gmail.com>
2025-11-04 01:27:34 -0300

    Atualizando dependências e ajustando configuração para ECMAScript modules

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,10 @@ export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.ts$': 'ts-jest', 
+    '^.+\\.ts$': 'ts-jest', // Para garantir que o TypeScript seja transformado corretamente
   },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
